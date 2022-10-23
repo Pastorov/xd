2ed66a123bc219185cbadbdee7e7925f0c19d256
Pastorov <pastor0v2000@gmail.com>
2022-10-23 20:20:33 +0200

    started

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Bird from './components/Bird';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Flappy Bird</Text>
+      <Bird />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/Bird.js
@@ -0,0 +1,19 @@
+import React from "react"
+import {StyleSheet, Text, View} from 'react-native'
+
+const Bird = () => {
+    return (
+        <View style={{
+            position: 'absolute',
+            backgroundColor: 'blue',
+            width: 50,
+            height: 50,
+
+        }}>
+
+
+        </View>
+    )
+}
+
+export default Bird
